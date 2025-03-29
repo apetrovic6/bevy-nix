f8098bee21f9e4b36fb25a21b0db3084e7dc19b3
apetrovic6 <petrovicante6@gmail.com>
2025-03-29 18:10:51 +0100

    update

---
+++ devenv.nix
@@ -0,0 +1,68 @@
+{ pkgs, lib, config, inputs, ... }:
+let 
+
+p = with pkgs; [
+    pkg-config
+    udev
+    alsa-lib 
+    vulkan-loader
+    xorg.libX11 
+    xorg.libXcursor 
+    xorg.libXi 
+    xorg.libXrandr # To use the x11 feature
+    libxkbcommon 
+    wayland
+  ];
+in {
+  # overlays = [
+  #   inputs.nixGL.overlay
+  # ];
+  
+  cachix.enable = false;
+  # https://devenv.sh/packages/
+  packages = p;
+
+  # https://devenv.sh/languages/
+  languages.rust = {
+    enable = true;
+    channel = "nightly";
+  };
+
+  env = {
+    LD_LIBRARY_PATH = lib.makeLibraryPath p;
+    NIXPKGS_ALLOW_UNFREE = 1;
+  };
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.run.exec = ''
+    nix run --impure github:nix-community/nixGL -- cargo run
+  '';
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
