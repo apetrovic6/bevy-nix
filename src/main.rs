3eaba92b7742f90ea92a4c38d8993ef7401d7a46
apetrovic6 <petrovicante6@gmail.com>
2025-03-29 18:08:34 +0100

    init commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::BLACK))
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, rotate)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+
+    commands.spawn(Sprite {
+        image: asset_server.load("bevy.png"),
+        ..default()
+    });
+    println!("ugala");
+}
+
+fn rotate(mut query: Query<&mut Transform, With<Sprite>>, time: Res<Time>) {
+    for mut bevy in &mut query {
+        bevy.rotate_local_z(-time.delta_secs());
+    }
+}
